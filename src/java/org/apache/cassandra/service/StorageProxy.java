d2098d81bb901607edd77d74776af173f95672b6
Matt Byrd <matthew_byrd@apple.com>
2024-11-20 20:55:55 -0800

    Use request start time where available from write response handler, also max the time offset with 0
    patch by Matt Byrd; reviewed by Chris Lohfink, Blake Eggleston for CASSANDRA-20014

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -2748,8 +2748,10 @@ public class StorageProxy implements StorageProxyMBean
                 }
 
                 long creationTime = currentTimeMillis();
-                if (DatabaseDescriptor.isUseCreationTimeForHintTtl()) {
-                    creationTime -= TimeUnit.MILLISECONDS.convert(Math.abs(approxTime.now() - mutation.getApproxCreatedAtNanos()), NANOSECONDS);
+                if (DatabaseDescriptor.isUseCreationTimeForHintTtl())
+                {
+                    long mutationCreationTimeNanos = responseHandler != null ? responseHandler.getRequestTime().startedAtNanos() : mutation.getApproxCreatedAtNanos();
+                    creationTime -= TimeUnit.MILLISECONDS.convert(Math.max(0, approxTime.now() - mutationCreationTimeNanos), NANOSECONDS);
                 }
                 logger.trace("Adding hints for {} with creation time {} ms", validTargets, creationTime);
                 HintsService.instance.write(hostIds, Hint.create(mutation,  creationTime));
